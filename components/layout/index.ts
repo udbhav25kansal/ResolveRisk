027108d55b323903a238e5970a4aac4bbb670741
udbhav <udbhav@UDBHAV>
2025-10-26 12:20:55 -0700

    Clean gh-pages branch - remove source files, keep only static build

--- components/layout/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Layout Components
- * Export all layout components for easy importing
- */
-
-export { Header } from './Header';
-export type { HeaderProps } from './Header';
-
-export { Footer } from './Footer';
